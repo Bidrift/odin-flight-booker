05bd751973490f2eaf1ed7124975261929994238
Bidrift <yassine.fighter@gmail.com>
2025-06-22 23:26:32 +0100

    Fix typo in enum status

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -2,5 +2,5 @@ class Flight < ApplicationRecord
   belongs_to :departure_airport, class_name: "Airport"
   belongs_to :arrival_airport, class_name: "Airport"
 
-  enum status: { ontime: 0, checkin: 1, boarding: 2, delayed: 3, cancelled: 4, departed: 5 }
+  enum :status, { ontime: 0, checkin: 1, boarding: 2, delayed: 3, cancelled: 4, departed: 5 }
 end
