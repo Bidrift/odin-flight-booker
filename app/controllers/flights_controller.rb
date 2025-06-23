6cc5b5d374a0cdc664c9c63e9696b08630e62f27
Bidrift <yassine.fighter@gmail.com>
2025-06-23 19:31:50 +0100

    Add passenger to controller Flights

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -5,6 +5,7 @@ class FlightsController < ApplicationController
         
         if flight_params
             @flight = Flight.new(flight_params)
+            @passengers = passenger_params[:passengers]
             flash.now['notice'] = 'You have searched something'
             if @flight.valid?(:search)
                 @flights = Flight.search_flights(@flight)
@@ -19,5 +20,9 @@ class FlightsController < ApplicationController
         params.expect(flight: [:departure_airport_id, :arrival_airport_id, :departure_time])
     rescue ActionController::ParameterMissing
         return false
+    end
+
+    def passenger_params
+        params.expect(flight: [:passengers])
     end
 end
