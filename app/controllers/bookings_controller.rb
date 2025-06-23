990092ec7d0835d443c26970744cb16500155759
Bidrift <yassine.fighter@gmail.com>
2025-06-23 23:51:22 +0100

    Add new Booking form

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,6 +1,13 @@
 class BookingsController < ApplicationController
     def new
-
+        unless booking_params
+            flash['alert'] = "Missing booking parameters, select flight and number of passengers"
+            redirect_to flights_path
+            return
+        end
+        @booking = Booking.new(flight_id: booking_params[:flight_id])
+        @passengers = Array.new
+        booking_params[:passengers].to_i.times { @passengers << @booking.passengers.build }
     end
 
     def create
@@ -9,5 +16,15 @@ class BookingsController < ApplicationController
 
     def show
 
+    end
+
+    private
+
+    def booking_params
+        params.expect(booking: [:flight_id, :passengers])
+        return false if params[:booking][:flight_id].nil? || params[:booking][:passengers].nil?
+        params.expect(booking: [:flight_id, :passengers])
+    rescue ActionController::ParameterMissing
+        return false
     end
 end
--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,13 @@
 class Booking < ApplicationRecord
   belongs_to :flight
   has_many :passengers
+
+  validate :has_passengers
+
+  private
+    def has_passengers
+      if passengers.none?
+        errors.add(:booking, "must have at least one passenger")
+      end
+    end
 end
