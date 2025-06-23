c1b63d92e3bf9e4af18aedce9d344729927e81ee
Bidrift <yassine.fighter@gmail.com>
2025-06-23 13:24:50 +0100

    Add search form

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,6 +1,21 @@
 class FlightsController < ApplicationController
 
     def index
+        @flight = Flight.new
+        
+        if flight_params
+            @flight = Flight.new(flight_params)
+            logger.debug(@flight)
+            flash.now['notice'] = 'You have searched something'
+        end
+        @airports = Airport.all.map { |x| [x.id, x.code] }
+    end
+    
+    private
 
+    def flight_params
+        params.expect(flight: [:departure_airport_id, :arrival_airport_id, :departure_time])
+    rescue ActionController::ParameterMissing
+        return false
     end
 end
