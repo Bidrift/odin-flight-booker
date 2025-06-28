d471c5ce2ab7f3d31af1ccd10dd522d74e52d9eb
Bidrift <yassine.fighter@gmail.com>
2025-06-28 01:38:55 +0100

    Add create and destroy Passengers

--- app/controllers/passengers_controller.rb
+++ app/controllers/passengers_controller.rb
@@ -1,2 +1,30 @@
 class PassengersController < ApplicationController
+    def create
+        @passenger = Passenger.new(passenger_params)
+        respond_to do |format|
+            if @passenger.save
+                format.turbo_stream { render turbo_stream: turbo_stream.append('passengers', @passenger)}
+            else
+                head(:unprocessable_entity)
+            end
+        end
+        
+    end
+
+    def destroy
+        @passenger = Passenger.find(params[:id])
+        respond_to do |format|
+            if @passenger.destroy
+                format.turbo_stream { render turbo_stream: turbo_stream.remove(@passenger)}
+            else
+                head(:unprocessable_entity)
+            end
+        end
+    end
+
+    private
+
+        def passenger_params
+            params.expect(passenger: [:name, :email])
+        end
 end
