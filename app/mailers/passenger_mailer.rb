550ecfea9f50d884dc9a115bee45cd0ca3fa1775
Bidrift <yassine.fighter@gmail.com>
2025-06-29 14:29:49 +0100

    Add booking confirmation mailers

--- app/mailers/passenger_mailer.rb
+++ app/mailers/passenger_mailer.rb
@@ -1,2 +1,9 @@
 class PassengerMailer < ApplicationMailer
+    default from: "notifications@example.com"
+
+    def confirm_booking
+        @passenger = params[:passenger]
+        @url = params[:url]
+        mail(to: @passenger.email, subject: "Booking Confirmation for Flight to "+@passenger.booking.flight.arrival_airport.code)
+    end
 end
