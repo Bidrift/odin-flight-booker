fc601617d10e55bc818656e0ff280709746ead0e
Bidrift <yassine.fighter@gmail.com>
2025-06-29 01:56:04 +0100

    Fix linting

--- app/controllers/passengers_controller.rb
+++ app/controllers/passengers_controller.rb
@@ -3,19 +3,18 @@ class PassengersController < ApplicationController
         @passenger = Passenger.new(passenger_params)
         respond_to do |format|
             if @passenger.save
-                format.turbo_stream { render turbo_stream: [turbo_stream.append('passengers-list', @passenger)]}
+                format.turbo_stream { render turbo_stream: [ turbo_stream.append("passengers-list", @passenger) ] }
             else
                 format.turbo_stream { head(:unprocessable_entity) }
             end
         end
-        
     end
 
     def destroy
         @passenger = Passenger.find(params[:id])
         respond_to do |format|
             if @passenger.destroy
-                format.turbo_stream { render turbo_stream: turbo_stream.remove(@passenger)}
+                format.turbo_stream { render turbo_stream: turbo_stream.remove(@passenger) }
             else
                 head(:unprocessable_entity)
             end
@@ -25,6 +24,6 @@ class PassengersController < ApplicationController
     private
 
         def passenger_params
-            params.expect(passenger: [:name, :email, :booking_id])
+            params.expect(passenger: [ :name, :email, :booking_id ])
         end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :flights, only: [ :index ]
   resources :bookings, only: [ :new, :create, :show ]
-  resources :passengers, only: [:create, :destroy]
+  resources :passengers, only: [ :create, :destroy ]
   root to: "flights#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
